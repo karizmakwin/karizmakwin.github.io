36ff3e8bbaa5c60fc246281bce48dd4fd900557f
karizmakwin <escora.mjane@gmail.com>
2024-01-28 22:27:54 +0800

    added project section

--- assets/scripts/home.js
+++
@@ -1,8 +0,0 @@
-
-const body = document.body
-const div = document.createElement("div")
-body.append(div)
-
-const strong = document.createElement("strong")
-strong.innerText = "Hello World"
-div.append(strong)
