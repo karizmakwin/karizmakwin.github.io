3967a95c2f6e5069670d195e91fe9689f463ac33
karizmakwin <escora.mjane@gmail.com>
2024-01-28 21:15:09 +0800

    initial commit

---
+++ assets/scripts/home.js
@@ -0,0 +1,8 @@
+
+const body = document.body
+const div = document.createElement("div")
+body.append(div)
+
+const strong = document.createElement("strong")
+strong.innerText = "Hello World"
+div.append(strong)
---
+++ assets/scripts/main.js
@@ -0,0 +1,42 @@
+console.log("Hello World!");
+console.log("Welcome to Web Dev 101");
+
+let myfirstVariable = 200;
+let mysecondVariable = 500;
+let result = myfirstVariable - mysecondVariable;
+console.log(result)
+
+function mySum(num1, num2) {
+    return num1 + num2
+}
+
+function mySub(num1, num2) {
+    return num1 - num2
+}
+
+function myDivide(num1, num2) {
+    return num1 / num2
+}
+
+function myMultiply(num1, num2) {
+    return num1 * num2
+}
+
+
+console.log(mySum(myfirstVariable,mysecondVariable))
+console.log(mySub(myfirstVariable,mysecondVariable))
+console.log(myDivide(myfirstVariable,mysecondVariable))
+console.log(myMultiply(myfirstVariable,mysecondVariable))
+
+let myIcecream = "Ube"
+if (myIcecream === "Ube") {
+    console.log("Yes, Ube is my fave ice cream")
+} else {
+    console.log("This is false")
+}
+
+/*for (let i = 0; i <= 100; i+=2){
+    console.log(i)
+} */
+
+
